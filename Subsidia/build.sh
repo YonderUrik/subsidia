7cea8f283bf0c9384e7e538ab20674815f2cc3c9
Daniele Roccaforte <64607921+YonderUrik@users.noreply.github.com>
2023-08-29 15:38:06 +0200

    Update build.sh

--- Subsidia/build.sh
+++ Subsidia/build.sh
@@ -6,27 +6,24 @@
 #!/bin/bash
 IMAGE_TYPE=$1
 VERSION=0.0.1
-REGISTRY_HOST=localhost:7452
 
 if [ "$IMAGE_TYPE" = "frontend" ]; then
     echo "Building frontend image. You have 5 seconds to stop the script"
     sleep 5
-    docker build ./frontend -f frontend/Dockerfile-prod -t $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
-    docker image push $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
+    docker build ./frontend -f frontend/Dockerfile-prod -t $IMAGE_TYPE:$VERSION
+    docker image push $IMAGE_TYPE:$VERSION
 elif [ "$IMAGE_TYPE" = "backend" ]; then
     echo "Building backend image. You have 5 seconds to stop the script"
     sleep 5
-    docker build ./backend -f backend/Dockerfile -t $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
-    docker image push $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
+    docker build ./backend -f backend/Dockerfile -t $IMAGE_TYPE:$VERSION
+    docker image push $IMAGE_TYPE:$VERSION
 elif [ "$IMAGE_TYPE" = "all" ]; then
-    echo "Building backend image. You have 5 seconds to stop the script"
+    echo "Building frontend/backend image. You have 5 seconds to stop the script"
     sleep 5
-    docker build ./backend -f backend/Dockerfile -t $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
-    docker image push $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
-    echo "Building frontend image. You have 5 seconds to stop the script"
-    sleep 5
-    docker build ./frontend -f frontend/Dockerfile-prod -t $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
-    docker image push $REGISTRY_HOST/apps/$IMAGE_TYPE:$VERSION
+    docker build ./backend -f backend/Dockerfile -t backend:$VERSION
+    docker build ./frontend -f frontend/Dockerfile-prod -t frontend:$VERSION
+    docker image push frontend:$VERSION
+    docker image push backend:$VERSION
 else
     echo "UNKOWN IMAGE TYPE: $IMAGE_TYPE"
 fi
