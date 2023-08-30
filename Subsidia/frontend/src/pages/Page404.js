071172dc98d29078248ebf5ec6ecedece8f02332
Daniele Roccaforte <64607921+YonderUrik@users.noreply.github.com>
2023-08-30 12:40:02 +0200

    update

--- Subsidia/frontend/src/pages/Page404.js
+++ Subsidia/frontend/src/pages/Page404.js
@@ -14,7 +14,7 @@ export default function Page404() {
   return (
     <>
       <Helmet>
-        <title> 404 Page Not Found | Convey apps</title>
+        <title> 404 Page Not Found | Subsidia</title>
       </Helmet>
 
       <MotionContainer>
